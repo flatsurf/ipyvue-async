42443886391a3ea0b45960deb54c21b421f669bd
Julian Rüth <julian.rueth@fsfe.org>
2021-10-09 23:23:02 +0200

    show frontend errors in the notebook

--- js/lib/comm.js
+++ js/lib/comm.js
@@ -57,6 +57,24 @@ export default {
   template: `
     <div>
       <slot />
+      <slot name="errors" v-bind:errors="errors">
+        <!--
+          Styling of error messages derived from Bootstrap, licensed under The MIT License (MIT):
+
+          Copyright (c) 2011-2018 Twitter, Inc.
+          Copyright (c) 2011-2018 The Bootstrap Authors
+
+          Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+          The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        -->
+        <div style="color: #a94442; background-color: #f2dede; border: 1px solid #ebccd1; padding: 15px; padding-right: 35px; border-radius: 4px; margin-top: 10px;" v-for="(error, i) of errors" :key="i">
+          {{ error }}
+          <button style="position: relative; top: -2px; right: -21px; color: inherit; padding: 0; cursor: pointer; float: right; opacity: .2; font-size: 21px; border: 0; background: 0 0;" @click="errors.splice(i, 1)">×</button>
+        </div>
+      </slot>
     </div>
   `,
   async mounted() {
@@ -80,38 +98,52 @@ export default {
     return {
       comm: null,
       tokens: {},
+      errors: [],
     };
   },
   props: ['refs'],
   methods: {
     onMessage(message) {
-      const payload = message.content.data;
-      const action = payload.action;
-      if (action === "call") {
-        const {target, endpoint, args} = payload.data;
-        this.call(target, endpoint, args)
-      } else if (action === "cancel") {
-        const { identifier } = payload.data;
-        this.cancel(identifier);
-      } else if (action === "query") {
-        const {identifier, data} = payload.data;
-        const {target, endpoint, args} = data;
-        this.query(identifier, target, endpoint, args);
-      } else {
-        throw new Error(`Unsupported action ${action}`);
+      try {
+        const payload = message.content.data;
+        const action = payload.action;
+        if (action === "call") {
+          const {target, endpoint, args} = payload.data;
+          this.call(target, endpoint, args)
+        } else if (action === "cancel") {
+          const { identifier } = payload.data;
+          this.cancel(identifier);
+        } else if (action === "query") {
+          const {identifier, data} = payload.data;
+          const {target, endpoint, args} = data;
+          this.query(identifier, target, endpoint, args);
+        } else {
+          throw new Error(`Unsupported action ${action}.`);
+        }
+      } catch (e) {
+        this.errors.push(e.message);
+        throw e;
       }
     },
 
-    call(target, endpoint, args) {
+    endpoint(target, method) {
       const ref = this.refs[target];
-      ref[endpoint](...args);
+      if (ref == null)
+        throw new Error(`No ref="${target}" found in component.`);
+      const endpoint = ref[method];
+      if (endpoint == null)
+        throw new Error(`No function ${method} found in component ${target}.`)
+      return endpoint;
+    },
+
+    call(target, endpoint, args) {
+      this.endpoint(target, endpoint)(...args);
     },
 
     async query(identifier, target, endpoint, args) {
-      const ref = this.refs[target];
       // TODO: Exception handling.
       // TODO: Await if promise.
-      let value = ref[endpoint]
+      let value = this.endpoint(target, endpoint);
       if (typeof value === "function")
         value = value(...args)
       // TODO: else if (args) complain.
